eaa2249a449ff26abf9cd1218ef39b0ab58d43cd
Samuel Hadden <shadden1107@gmail.com>
2020-05-13 13:38:53 -0400

    udated quadrature routines for sk and Dsk coefficients

--- celmech/miscellaneous.py
+++ celmech/miscellaneous.py
@@ -1,30 +1,56 @@
 import numpy as np
-from scipy.special import k0,k1
+from scipy.special import k0,k1,p_roots
 
-def sk(k,y,Nquad = 40):
+def sk(k,y,tol=1.49e-08,rtol=1.49e-08,maxiter=50,miniter=1):
     """
-    Approximate disturibing function
-    coefficient described in 
+    Approximate disturibing function coefficient described in 
     Hadden & Lithwick (2018) [#]_
-    
+
     .. [#] `ADS link <https://ui.adsabs.harvard.edu/abs/2018AJ....156...95H/abstract>`
     
+    Quadrature routine based on scipy.quadrature.
+
     Arguments
     ---------
     k : int
         Order of resonance
     y : float
         e / ecross; must be y<1
-    Nquad : int, optional
-        Number of quadrature points to use to 
-        determine integral.
+    tol, rtol: float, optional
+        Control absolute and relative tolerance of integration.
+        Iteration stops when error between last two iterates 
+        is less than `tol` OR the relative change is less than 
+        `rtol`.
+    maxiter : int, optional
+        Maximum order of Gaussian quadrature.
+    miniter : int, optional
+        Minimum order of Gaussian quadrature
+
     Returns
     -------
-    float
+    val : float
+        Gaussian quadrature approximation of s_k(y)
     """
-    assert y<1
+    if y>1:
+        raise ValueError("sk(k,y) called with y={:f}."
+        "Value of y must be less than 1.")
+    maxiter=max(miniter+1,maxiter)
+    val = np.inf
+    err = np.inf
+    for n in xrange(miniter,maxiter+1):
+        newval = _sk_integral_fixed_quad(k,y,n)
+        err = abs(newval-val)
+        val = newval
+        if err<tol or err< rtol*abs(val):
+            break
+    else:
+        warnings.warn("maxiter (%d) exceeded. Latest difference = %e" % (maxiter, err),
+            AccuracyWarning)
+    return val
+def _sk_integral_fixed_quad(k,y,Nquad):
+
     # Get numerical quadrature nodes and weight
-    nodes,weights = np.polynomial.legendre.leggauss(N_QUAD_PTS)
+    nodes,weights = p_roots(Nquad)
     
     # Rescale for integration interval from [-1,1] to [-pi,pi]
     nodes = nodes * np.pi
@@ -32,4 +58,64 @@ def sk(k,y,Nquad = 40):
     arg1 = 2 * k * (1 + y * np.cos(nodes)) / 3
     arg2 = k * nodes + 4 * k * y * np.sin(nodes) / 3
     integrand = k0(arg1) * np.cos(arg2)
+    return integrand @ weights
+
+def Dsk(k,y,tol=1.49e-08,rtol=1.49e-08,maxiter=50,miniter=1):
+    """
+    Derivative of disturibing function coefficient s_k
+    with respect to argument y. Coefficients are described 
+    in Hadden & Lithwick (2018) [#]_
+
+    .. [#] `ADS link <https://ui.adsabs.harvard.edu/abs/2018AJ....156...95H/abstract>`
+    
+    Quadrature routine based on scipy.quadrature.
+
+    Arguments
+    ---------
+    k : int
+        Order of resonance
+    y : float
+        e / ecross; must be y<1
+    tol, rtol: float, optional
+        Control absolute and relative tolerance of integration.
+        Iteration stops when error between last two iterates 
+        is less than `tol` OR the relative change is less than 
+        `rtol`.
+    maxiter : int, optional
+        Maximum order of Gaussian quadrature.
+    miniter : int, optional
+        Minimum order of Gaussian quadrature
+
+    Returns
+    -------
+    val : float
+        Gaussian quadrature approximation of s_k(y)
+    """
+    if y>1:
+        raise ValueError("sk(k,y) called with y={:f}."
+        "Value of y must be less than 1.")
+    maxiter=max(miniter+1,maxiter)
+    val = np.inf
+    err = np.inf
+    for n in xrange(miniter,maxiter+1):
+        newval = _Dsk_integral_fixed_quad(k,y,n)
+        err = abs(newval-val)
+        val = newval
+        if err<tol or err< rtol*abs(val):
+            break
+    else:
+        warnings.warn("maxiter (%d) exceeded. Latest difference = %e" % (maxiter, err),
+            AccuracyWarning)
+    return val
+def _Dsk_integral_fixed_quad(k,y,Nquad):
+
+    # Get numerical quadrature nodes and weight
+    nodes,weights = p_roots(Nquad)
+    
+    # Rescale for integration interval from [-1,1] to [-pi,pi]
+    nodes = nodes * np.pi
+    weights = weights * 0.5
+    arg1 = 2 * k * (1 + y * np.cos(nodes)) / 3
+    arg2 = k * nodes + 4 * k * y * np.sin(nodes) / 3
+    integrand = -2 * k * k1(arg1) * np.cos(nodes) * np.cos(arg2) / 3 - 4 * k * k0(arg1) * np.sin(nodes) * np.sin(arg2) / 3
     return integrand @ weights
