e090bb78c378fa28159825fbe42a9421e5ab83ba
Samuel Hadden <shadden1107@gmail.com>
2020-05-19 21:40:29 -0400

    averaged resonances equations now loaded by default

--- celmech/__init__.py
+++ celmech/__init__.py
@@ -37,5 +37,6 @@ except:
 
 from .andoyer import Andoyer, AndoyerHamiltonian
 from .poincare import PoincareParticle, Poincare, PoincareHamiltonian
+from .MeanMotionResonance.PlanarMMR import ResonanceEquations
 
-__all__ = ["Andoyer", "AndoyerHamiltonian", "PoincareParticle", "Poincare", "PoincareHamiltonian"]
+__all__ = ["Andoyer", "AndoyerHamiltonian", "PoincareParticle", "Poincare", "PoincareHamiltonian","ResonanceEquations"]
