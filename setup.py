abdeb06308b950f7f9b7aceca6c421158e0400d5
Daniel Tamayo <tamayo.daniel@gmail.com>
2021-06-28 17:25:37 -0400

    Updating version to 0.3.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ try:
     ghash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii")
     ghash_arg = "-DCELMECHGITHASH="+ghash.strip()
 except:
-    ghash_arg = "-DCELMECHGITHASH=1a571053cc3474000f200dc2cdd9e7e80616aedb" #GITHASHAUTOUPDATE
+    ghash_arg = "-DCELMECHGITHASH=349cd94e230602e9e9fa6e56a8b5efb5904df738" #GITHASHAUTOUPDATE
 
 extra_link_args=[]
 if sys.platform == 'darwin':
@@ -49,7 +49,7 @@ if not os.getenv('READTHEDOCS'):
         install_requires = packages
 
 setup(name='celmech',
-    version='0.3.3',
+    version='0.3.4',
     description='Open source tools for celestial mechanics',
     url='http://github.com/shadden/celmech',
     author='Dan Tamayo, Sam Hadden',
