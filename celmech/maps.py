313cbada66751cbb49f50b7ae5c49fbb5a223b1d
Sam Hadden <shadden1107@gmail.com>
2022-03-03 17:01:51 -0500

    update funcs for stable/unstable manifold plotting

--- celmech/maps.py
+++ celmech/maps.py
@@ -386,4 +386,236 @@ class EncounterMap():
         return T
 
 
+from sympy import bell
+from scipy.special import binom
+def _evaluate_chain_rule(n,farr,garr):
+    r"""
+    Evaluate the :math:`n`th derivative of :math:`f(g(x))` at x=0
+    by specifying the Taylor series coefficients of the functions
+    f and g.
 
+    Arguments
+    ---------
+    farr : numpy array
+        Taylor series coefficients of function f such that:
+        f(u) = f[0] + f[1]*u + f[2]*u^2/2 + ... + f[n]*u^n/n! + ...
+    garr : numpy array
+        Taylor series coefficients of function g, such that:
+        g(x) = g[0] + g[1]*x + g[2]*x^2/2 + ... + g[n]*x^n/n! + ...
+
+    Returns
+    -------
+    float
+        The value of :math:`\frac{d^n}{dx^n}f(g(x))\bigg|_{x=0}`
+    """
+    #
+    return np.sum([farr[k] * bell(n,k,garr[1:n-k+2]) for k in range(1,n)])
+
+def _to_uv_derivs(T,n,m,c,s):
+    """
+    Helper function for rotate_derivs_array
+    """
+    tot = np.zeros(2)
+    R = np.array([[c,s],[-s,c]])
+    for l in range(n+1):
+        binom_nl = binom(n,l)
+        for l1 in range(m+1):
+            cfactor = c**(m+l-l1)
+            sfactor = s**(n-l+l1)
+            coeff = (-1)**l1 * binom_nl * binom(m,l1) * cfactor * sfactor
+            tot +=  coeff * T[:,l+l1,n+m-l-l1]
+    return R @ tot
+
+def rotate_derivs_array(T,theta_rot):
+    r"""
+    Given the partial derivatives of a map, :math:`T(x,y)`, with respect
+    to :math:`x` and :math:`y`, get the partial derivative with respect to new,
+    rotated coordinates :math:`(u,v)` defined by
+
+    .. math::
+        \begin{pvec}
+            u \\
+            v
+        \end{pvec}
+        =
+        R_\theta \cdot
+        \begin{pvec}
+            x \\
+            y
+        \end{pvec}
+
+    where :math:`R_\theta` is a rotation matrix.
+    """
+    c,s = np.cos(theta_rot),np.sin(theta_rot)
+    T1 = np.zeros(T.shape)
+    for n in range(1,T.shape[1]):
+        for l in range(n+1):
+            T1[:,l,n-l] = _to_uv_derivs(T,l,n-l,c,s)
+    return T1
+
+### Utilities for calculating Psis ####
+from collections import defaultdict
+def deriv_of_coeff_and_pows(coeff,pows):
+    new_pows = np.append(pows,0)
+    results = []
+    for m in range(len(pows)):
+        if new_pows[m]>0:
+            new_pows = np.append(pows,0)
+            new_pows[m]-=1
+            new_pows[m+1]+=1
+            results.append((coeff * pows[m], new_pows))
+    return results
+
+def _consolidate_coeff_and_pows_list(cp_list):
+    pows_arr = np.array([_[1] for _ in cp_list])
+    coeff_arr = np.array([_[0] for _ in cp_list])
+    i = 0
+    tot = 0
+    result = []
+    while tot < len(coeff_arr):
+        pows = pows_arr[i]
+        msk = np.alltrue(pows_arr==pows,axis=1)
+        coeff = np.sum(coeff_arr[msk])
+        result+=[(coeff,pows)]
+        # next index of unique powers
+        tot += np.sum(msk)
+        i=i+np.argmin(msk[i:])
+    return result
+
+
+def get_Psi_dicts(n):
+    PsiOld = {
+        (0,1):[(1,np.array([0,1]))],
+        (1,0):[(1,np.array([0,0]))]
+    }
+    Psis = [0,PsiOld]
+    for m in range(1,n):
+        PsiNew = defaultdict(list)
+        for ij,coeffs_and_pows in PsiOld.items():
+            i,j = ij
+            PsiNew[(i+1,j)] += coeffs_and_pows
+            for coeff,pows in coeffs_and_pows:
+                PsiNew[(i,j)] += deriv_of_coeff_and_pows(coeff,pows)
+                add_one = pows.copy()
+                add_one[1] +=1
+                PsiNew[(i,j+1)] += [(coeff,add_one)]
+        PsiOld = PsiNew.copy()
+        for ij, lst in PsiOld.items():
+            PsiOld[ij] = _consolidate_coeff_and_pows_list(lst)
+        Psis.append(PsiOld)
+    # Remove (0,1) entries from Psis
+    [Psi.pop((0,1),None) for Psi in Psis[1:]]
+    return Psis
+
+def evaluate_Psi(Psi_dict,Tprimes_arr,farr):
+    tot = 0
+    for ij,coeffs_and_pows_list in Psi_dict.items():
+        i,j = ij
+        Tprime_ij = Tprimes_arr[i,j]
+        sub_tot = 0
+        for coeff,pows in coeffs_and_pows_list:
+            if coeff==0:
+                continue
+            npows = pows.shape[0]
+            sub_tot += coeff * np.product(farr[:npows]**pows)
+        tot+= Tprime_ij * sub_tot
+    return tot
+
+def func_from_series(coeffs,x):
+    """
+    Given a set of Taylor series coefficients, (c_0,....,c_N), evalute
+    the sum
+
+    .. math::
+        \sum_{n=0}^{N} c_n x^n / n!
+
+    Arguments
+    ---------
+    coeffs : numpy array
+        Values of Taylor series coeffieciens
+    x : float
+        Argument of function
+
+    Returns
+    -------
+    float
+    """
+    return coeffs @ np.array([x**n/np.math.factorial(n) for n in range(coeffs.shape[0])])
+
+def manifold_approx(u,n,farr,garr):
+    f = lambda x: func_from_series(farr[:n+1],x)
+    g = lambda x: func_from_series(garr[:n+1],x)
+    p0 = np.array([u,f(u)])
+    p1 = np.array([g(u),f(g(u))])
+    return p0,p1
+
+def solve_manifold_f_and_g(xunst,mapobj,Nmax,unstable=True):
+    r"""
+    Solve for Taylor series approximations of functions
+    :math:`f` and :math:`g` satisfying
+
+    .. math::
+        T(x_u(s)) = x_* + g(s)\hat{x}_u + f(g(s))\hat{x}_\perp
+
+    where ...
+
+    Arguments
+    ---------
+    xunst : array-like, (2,)
+        Unstable fixed point of map.
+    mapobj : object
+        A 2D map.
+    Nmax : int
+        Maximum order Taylor series coefficients to
+        compute for f and g
+    unstable : bool, optional
+        If true, solve for the unstable manifold f and
+        g functions, otherwise solve for the stable manifold
+        functions.
+    Returns
+    -------
+    R : ndarray, shape (2,2)
+        Rotation matrix
+    """
+    # Array of partial derivatives up to order Nmax
+    if unstable:
+        T = mapobj.partial_derivs(xunst,Nmax)
+    else:
+        T = mapobj.inv_partial_derivs(xunst,Nmax)
+    # jacobian evaluated at xunst
+    jac = np.array([
+        [T[0][1,0],T[0][0,1]],
+        [T[1][1,0],T[1][0,1]]
+    ])
+    vals,vecs = np.linalg.eig(jac)
+    isreal = np.logical_not(np.iscomplex(vals))
+    assert np.alltrue(isreal), "Eigenvalues of map at point ({},{}) are\
+    complex!".format(xunst[0],xunst[1])
+    iunst = np.argmax(vals)
+    # Unstable eigenvalue and direction
+    lambdaU = vals[iunst]
+    uvec = vecs[:,iunst]
+    theta_rot = np.arctan2(uvec[1],uvec[0])
+    # Rotation matrix
+    s,c = np.sin(theta_rot), np.cos(theta_rot)
+    R = np.array([[c,s],[-s,c]])
+
+    Tprime = rotate_derivs_array(T,theta_rot)
+    # Initialize iterative procedure to solve for 
+    # f and g arrays
+    TU_01 = Tprime[0][0,1]
+    Tperp_01 = Tprime[1][0,1]
+    farr,garr = np.zeros((2,Nmax+1))
+    garr[1] = lambdaU
+    Psis = get_Psi_dicts(Nmax)
+    # Iteratively solve for f and g coeffs
+    for n in range(2,Nmax + 1):
+        Psi_dict=Psis[n]
+        # Get numerical value of Psis
+        PsiPerp = evaluate_Psi(Psi_dict,Tprime[1],farr)
+        PsiU = evaluate_Psi(Psi_dict,Tprime[0],farr)
+        denom = Tperp_01-lambdaU**n
+        Bsum = _evaluate_chain_rule(n,farr,garr)
+        farr[n] = (Bsum-PsiPerp)/denom
+        garr[n] = TU_01 * farr[n] + PsiU
+    return R, farr, garr
