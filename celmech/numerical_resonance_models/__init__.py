27389a57c3b55f764eb94d40677ce5dc9785222d
Samuel Hadden <shadden1107@gmail.com>
2022-05-10 13:06:55 -0400

    restructred numerical resonance models

--- celmech/numerical_resonance_models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-"""celmech tools for numerically averaged resonance equations."""
-from .planarMMR import PlanarResonanceEquations
-from .spatialMMR import SpatialResonanceEquations
-__all__ = ["PlanarResonanceEquations","SpatialResonanceEquations"]
